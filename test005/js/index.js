c651354989473fe3f4ca2347cadf611815476c19
1−10 谷 <tani@1-10.com>
2018-02-08 00:14:13 +0900

    [update]途中

--- test005/js/index.js
+++ test005/js/index.js
@@ -1,44 +1,21 @@
-var VALUE_SKETCH = (function(){
-  var self = {};
-
-  self.x = 0;
-  self.y = 0;
-  self.degree = 0;
-  self.radian = 0;
-
-  self.x_init = 150;
-  self.y_init = 150;
-  self.radius = 50;
-
-  self.x_move = 1;
-  self.y_move = 90;
-  self.degree_move = 3;
-
-  return self;
-})();
-
 function setup() {
-  var WIDTH = window.innerWidth,
-      HEIGHT = window.innerHeight;
-
-  var v = VALUE_SKETCH;
-  v.x_init = window.innerWidth / 2;
-  v.y_init = window.innerHeight / 2;
-
-  createCanvas(WIDTH, HEIGHT);
+  var canvas = createCanvas(400, 400);
+  canvas.parent('canvas_area');
 }
 
 function draw() {
-  var v = VALUE_SKETCH;
 
-  v.radius += 1;
-
-  v.degree += v.degree_move;
-  v.radian = v.degree * Math.PI / 180;
-
-  v.x = Math.cos(v.radian) * v.radius + v.x_init;
-  v.y = Math.sin(v.radian) * v.radius + v.y_init;
-
-  //background(255);
-  ellipse(v.x, v.y, 50, 50);
 }
+
+var Walker = function() {
+  this.x;
+  this.y;
+  this.construct();
+};
+
+Walker.prototype = {
+  construct: function() {
+    
+  }
+  
+};
