3e1f5bde273b37d5c273336671ab3911a575c952
1−10 谷 <tani@1-10.com>
2018-02-19 20:28:27 +0900

    [Add]p5.Vectorを利用してベクトルを記述

--- test006/js/index.js
+++ test006/js/index.js
@@ -1,29 +1,29 @@
-var Walker = function() {
-  this.x;
-  this.y;
-  this.construct();
-};
-
-Walker.prototype = {
-  construct: function() {
-    this.x = window.innerWidth;
-    this.y = window.innerHeight;
-  },
-
-  display: function() {
-    
-  },
-  
-  step: function() {
-    
-  }
-};
+var x = 100;
+var y = 100;
+var xspeed = 5;
+var yspeed = 3.3;
 
 function setup() {
-  var canvas = createCanvas(800, 550);
+  var canvas = createCanvas(window.innerWidth, window.innerHeight);
   canvas.parent('canvas_area');
+  background(0);
 }
 
 function draw() {
+  background(0);
 
+  x += xspeed;
+  y += yspeed;
+
+  if((x > width) || (x < 0)) {
+    xspeed = xspeed * -1;
+  }
+
+  if((y > height) || (y < 0)) {
+    yspeed = yspeed * -1;
+  }
+
+  stroke(0);
+  fill(175);
+  ellipse(x, y, 16, 16);
 }
--- test007/js/index.js
+++ test007/js/index.js
@@ -1,29 +1,32 @@
-var x = 100;
-var y = 100;
-var xspeed = 5;
-var yspeed = 3.3;
+var v1, velocity;
 
 function setup() {
   var canvas = createCanvas(window.innerWidth, window.innerHeight);
   canvas.parent('canvas_area');
   background(0);
+
+  v1 = createVector(40, 50);
+  velocity = createVector(5, 3.3);
+
+  console.log(v1);
+  console.log(velocity);
 }
 
 function draw() {
   background(0);
 
-  x += xspeed;
-  y += yspeed;
+  v1.x += velocity.x;
+  v1.y += velocity.y;
 
-  if((x > width) || (x < 0)) {
-    xspeed = xspeed * -1;
+  if((v1.x > width) || (v1.x < 0)) {
+    velocity.x = velocity.x * -1;
   }
 
-  if((y > height) || (y < 0)) {
-    yspeed = yspeed * -1;
+  if((v1.y > height) || (v1.y < 0)) {
+    velocity.y = velocity.y * -1;
   }
 
   stroke(0);
   fill(175);
-  ellipse(x, y, 16, 16);
+  ellipse(v1.x, v1.y, 16, 16);
 }
