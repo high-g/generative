b80208be9684bb9c5143beff0cdfaa4cb210a8d7
1−10 谷 <tani@1-10.com>
2018-02-19 20:14:41 +0900

    [update]基礎レベル写経

--- test007/js/index.js
+++ test007/js/index.js
@@ -1,8 +1,29 @@
+var x = 100;
+var y = 100;
+var xspeed = 5;
+var yspeed = 3.3;
+
 function setup() {
-  var canvas = createCanvas(800, 550);
+  var canvas = createCanvas(window.innerWidth, window.innerHeight);
   canvas.parent('canvas_area');
+  background(0);
 }
 
 function draw() {
+  background(0);
 
+  x += xspeed;
+  y += yspeed;
+
+  if((x > width) || (x < 0)) {
+    xspeed = xspeed * -1;
+  }
+
+  if((y > height) || (y < 0)) {
+    yspeed = yspeed * -1;
+  }
+
+  stroke(0);
+  fill(175);
+  ellipse(x, y, 16, 16);
 }
