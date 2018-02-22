e5235073d7017c5408c6528c42a2ae02db6fccb4
1−10 谷 <tani@1-10.com>
2018-02-22 12:21:22 +0900

    [update]加速度追加

--- test010/js/Mover.js
+++ test010/js/Mover.js
@@ -6,6 +6,8 @@
 var Mover = function() {
   this.location;
   this.velocity;
+  this.acceleration;
+  this.topspeed;
   this.width;
   this.height;
   this.constructor();
@@ -16,12 +18,17 @@ var p = Mover.prototype;
 p.constructor = function() {
   this.width = innerWidth;
   this.height = innerHeight;
-  this.location = createVector(random(this.width), random(this.height));
-  this.velocity = createVector(random(-20, 20), random(-20, 20));
+  this.location = createVector(this.width / 2, this.height / 2);
+  this.velocity = createVector(0,0);
+  this.acceleration = createVector(-0.001, 0.01);
+  this.topspeed = 100;
 }
 
 p.update = function() {
+  this.velocity.add(this.acceleration);
+  this.velocity.limit(this.topspeed);
   this.location.add(this.velocity);
+
 };
 
 p.display = function() {
