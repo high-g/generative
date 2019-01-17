dffcc022278921a4d8df579e07d066b4cf2e9143
1−10 谷 <tani@1-10.com>
2019-01-17 16:29:33 +0900

    [update]

---
+++ test011/src/components/PixiCanvas.vue
@@ -0,0 +1,36 @@
+<template>
+  <canvas id="canvas"></canvas>
+</template>
+
+<script>
+import * as PIXI from 'pixi.js'
+
+export default {
+  data () {
+    return {
+      renderer: null,
+      stage: null,
+      
+    }
+  },
+  methods: {
+    init () {
+    },
+    fill () {
+    },
+    count () {
+    },
+    render () {
+      window.requestAnimationFrame(this.render)
+    }
+  },
+  mounted () {
+  }
+}
+</script>
+
+<style scoped lang="scss">
+#canvas {
+  background: #000;
+}
+</style>
