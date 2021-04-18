155c5a3b2293744ce9c64f2361fec71683867842
tani <tani@1-10.com>
2021-04-18 10:41:44 +0900

    Update ユークリッド互除法

--- 210417/sketch.js
+++ 210417/sketch.js
@@ -1,8 +1,22 @@
-let num = 1
+let numa = 100
+let numb = 60
+let numc
+let count = 0
+let gcf
 
 function setup() {
-  console.log(num)
-  console.log(num++)
+  Euclid(numa, numb)
+  console.log(gcf)
 }
 
 function draw() {}
+
+function Euclid(num1, num2) {
+  numc = num1 % num2
+  if (numc === 0) {
+    gcf = num2
+  } else {
+    count++
+    Euclid(num2, numc)
+  }
+}
